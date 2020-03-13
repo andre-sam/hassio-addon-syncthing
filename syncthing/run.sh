9b5ed2b9c6665471c4e2af4bd9d2be232d3baf3b
Andre <61034282+andre-sam@users.noreply.github.com>
2020-03-14 01:15:36 +1100

    Update run.sh

--- syncthing/run.sh
+++ syncthing/run.sh
@@ -4,4 +4,4 @@ set -e
 if [ ! -f '/share/syncthing/config/config.xml' ]; then
     syncthing -generate=/share/syncthing/config
 fi
-syncthing -no-browser -gui-address="0.0.0.0:8384" -home="/share/syncthing/config"
+syncthing -no-browser -gui-address="0.0.0.0:8384" -home="/share/syncthing/config" -no-restart -logflags=0 | grep -v "INFO: "
