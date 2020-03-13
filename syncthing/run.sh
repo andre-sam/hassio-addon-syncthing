e191d6dcfd3ecc17fc38fe0bc7f1c686aea15af5
Andre <61034282+andre-sam@users.noreply.github.com>
2020-03-13 23:51:41 +1100

    Update run.sh

--- syncthing/run.sh
+++ syncthing/run.sh
@@ -3,6 +3,5 @@ set -e
 
 if [ ! -f '/share/syncthing/config/config.xml' ]; then
     syncthing -generate=/share/syncthing/config
-    sed -i 's|<address>127.0.0.1:8384</address>|<address>:8384</address>|' /share/syncthing/config/config.xml
 fi
-syncthing -no-browser -paths -home=/share/syncthing/config
+syncthing -no-browser -paths -gui-address=0.0.0.0:8384 -home=/share/syncthing/config
