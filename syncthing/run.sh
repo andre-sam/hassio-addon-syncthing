91e7104292adc6a4c477bbfe19985149d5a4d662
Andre <61034282+andre-sam@users.noreply.github.com>
2020-03-13 20:52:35 +1100

    Create run.sh

---
+++ syncthing/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+if [ ! -f '/share/syncthing/config/config.xml' ]; then
+    syncthing -generate=/share/syncthing/config
+    sed -i 's|<address>127.0.0.1:8384</address>|<address>:8384</address>|' /share/syncthing/config/config.xml
+fi
+syncthing -no-browser -home=/share/syncthing/config
